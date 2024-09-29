85469d240639a73f37da7436fcb8a29d43a99940
Stephen Johnston <stephenjohnston@protonmail.com>
2024-09-28 20:39:15 -0700

    Initial commit.

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div id="workstation">
+    <div id="monitor">
+      <div id="screen">$ HELLO</div>
+    </div>
+    <div id="monitor--panel">
+      <div class="monitor--panel__left"></div>
+      <div class="monitor--panel__center"></div>
+      <div class="monitor--panel__right">
+        <label>video monitor <span>model T1000</span></label>
+        <button data-label="power"></button>
+      </div>
+    </div>
+  </div>
+`
+document.addEventListener('DOMContentLoaded', () => {
+  const powerButton = document.getElementById("powerButton");
+  const screen = document.getElementById("screen");
+  powerButton?.addEventListener('click', () => {
+    
+  });
+});
